fd8f815d30825e8facd7c01f2a9e450ebd7a0189
Murilo Cunha <murilo@dataroots.io>
2021-08-06 23:42:30 +0200

    add tests for data models

---
+++ tests/test_data_models.py
@@ -0,0 +1,99 @@
+"""Test data models and methods"""
+from nbhelp.data_models import (
+    Cell,
+    CellMetadata,
+    JupyterNotebook,
+    KernelSpec,
+    NotebookMetadata,
+)
+
+
+class TestNotebookMetadata:
+    @property
+    def kernel_spec(self):
+        return KernelSpec(display_name="kernel_display_name", name="kernel_name")
+
+    @property
+    def notebook_metadata(self):
+        return NotebookMetadata(
+            kernelspec=self.kernel_spec, field_to_remove="Field to remove"
+        )
+
+    def test_remove_extra_fields(self):
+        metadata = self.notebook_metadata
+        assert hasattr(metadata, "field_to_remove")
+        metadata.remove_extra_fields()
+        assert metadata == NotebookMetadata(
+            kernelspec=self.kernel_spec, field_to_remove=None
+        )
+
+
+class TestCell:
+    @property
+    def cell_metadata(self):
+        return CellMetadata(field_to_remove="Field to remove")
+
+    @property
+    def cell(self):
+        return Cell(
+            cell_type="code",
+            metadata=self.cell_metadata,
+            source="test_source",
+            execution_count=1,
+        )
+
+    def test_cell_metadata(self):
+        metadata = self.cell_metadata
+        metadata.remove_extra_fields()
+        assert metadata.dict(exclude_none=True) == {}
+        assert metadata.dict(exclude_none=False) == {"field_to_remove": None}
+        assert metadata == CellMetadata(field_to_remove=None)
+
+    def test_clear(self):
+        cell = self.cell
+        assert cell.metadata is not None
+        cell.clear(metadata=True, execution_count=True, outputs=True)
+        assert cell == Cell(
+            cell_type="code",
+            metadata=CellMetadata(),
+            outputs=[],
+            source="test_source",
+            execution_count=None,
+        )
+
+    def test_clean_dict(self):
+        cell = self.cell
+        assert cell.clean_dict() == {
+            "cell_type": "code",
+            "metadata": {},
+            "outputs": [],
+            "source": "test_source",
+            "execution_count": None,
+        }
+
+
+class TestJupyterNotebook(TestNotebookMetadata, TestCell):
+    @property
+    def jupyter_notebook(self):
+        return JupyterNotebook(
+            metadata=self.notebook_metadata,
+            nbformat=4,
+            nbformat_minor=4,
+            cells=[self.cell] * 2,
+        )
+
+    def test_clear_metadata(self):
+        notebook = self.jupyter_notebook
+        notebook.clear_metadata(notebook=True, cells=True)
+        assert notebook.metadata == NotebookMetadata(
+            field_to_remove=None,
+            kernelspec=KernelSpec(
+                display_name="kernel_display_name", name="kernel_name"
+            ),
+        )
+        assert notebook.metadata.dict(exclude_none=True) == {
+            "kernelspec": {"display_name": "kernel_display_name", "name": "kernel_name"}
+        }
+        assert all(cell.metadata == CellMetadata() for cell in notebook.cells)
+        assert all(cell.outputs == [] for cell in notebook.cells if cell.cell_type == 'code')
+        assert all(cell.execution_count is None for cell in notebook.cells if cell.cell_type == 'code')
