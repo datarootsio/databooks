267aac49d8d3d7b6ef9a270c0a28f71a3c5abc6e
Murilo Cunha <murilo@dataroots.io>
2021-10-27 22:42:08 +0200

    add logger and common data type for paths

---
+++ databooks/common.py
@@ -0,0 +1,21 @@
+"""Common set of miscellaneous functions"""
+import logging
+from pathlib import Path
+from typing import Union
+
+from rich.logging import RichHandler
+
+FilePath = Union[Path, str]
+
+
+def get_logger(name: str, level: str = "INFO") -> logging.Logger:
+    """Get logger with rich configuration."""
+    logging.basicConfig(
+        level=level,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)],
+    )
+
+    logger = logging.getLogger(name)
+    return logger
