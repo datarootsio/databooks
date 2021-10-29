d78c9f6d63fd0ff60e7c158134ead19dc378935e
Murilo Cunha <murilo@dataroots.io>
2021-10-29 23:01:35 +0200

    rename test file to reflect modulee

--- tests/test_git_utils.py
+++ tests/test_git_utils.py
@@ -2,8 +2,9 @@ from databooks.git_utils import get_repo
 from pathlib import Path
 from git import Repo
 
+
 def test_get_repo():
-    path = Path(__file__)
-    repo = get_repo(path)
+    """Find git repository"""
+    repo = get_repo(__file__)
     assert isinstance(repo, Repo)
     assert Path(repo.working_dir).stem == "databooks"
