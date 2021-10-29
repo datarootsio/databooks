8495d208bdb3b744dd5cb7bd3ef2364e40ccfe12
Murilo Cunha <murilo@dataroots.io>
2021-10-29 23:00:10 +0200

    use recursive built in and log git repo

--- databooks/git_utils.py
+++ databooks/git_utils.py
@@ -4,12 +4,11 @@ from .common import FilePath, get_logger
 
 logger = get_logger(name=__file__)
 
+
 def get_repo(path: FilePath = None):
     """Find git repo in current or parent directories"""
     if path is None:
         path = Path.cwd()
-    logger.info(path)
-    try:
-        return Repo(path)
-    except InvalidGitRepositoryError:
-        return get_repo(path.parent)
+    repo = Repo(path=path, search_parent_directories=True)
+    logger.info(f"Repo found at: {repo.working_dir}")
+    return repo
