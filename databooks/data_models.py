d2904db1cc79de4692579b89f828179598f5b7ab
Murilo Cunha <murilo@dataroots.io>
2021-11-08 18:35:28 +0100

    allow for removing arbitrary metadata

--- databooks/data_models.py
+++ databooks/data_models.py
@@ -1,37 +1,39 @@
 """Data models - Pydantic models for Jupyter notebook components"""
-from typing import List, Optional, Union
+from collections import abc
+from typing import Iterable, List, Optional, Sequence, Union
 
 from pydantic import BaseModel, Extra, PositiveInt, validator
 
 
-class BaseModelWithRemoveExtras(BaseModel):
+class BaseModelWithExtras(BaseModel):
+    """Base Pydantic class with extras on managing different fields."""
+
+    def remove_fields(self, fields: Iterable[str]):
+        """Remove selected fields."""
+        for field in fields:
+            delattr(self, field)
+
     def remove_extra_fields(self) -> None:
         """Remove extra fields"""
         fields_to_remove = tuple(
             field for field, _ in self if field not in self.__fields__
         )
-        for field in fields_to_remove:
-            delattr(self, field)
+        self.remove_fields(fields_to_remove)
 
 
-class KernelSpec(BaseModel):
-    display_name: str
-    name: str
-
-
-class NotebookMetadata(BaseModelWithRemoveExtras, extra=Extra.allow):
-    kernelspec: Optional[KernelSpec]
-
-
-class CellMetadata(BaseModelWithRemoveExtras, extra=Extra.allow):
+class NotebookMetadata(BaseModelWithExtras, extra=Extra.allow):
     ...
 
 
-class CellOutputs(BaseModelWithRemoveExtras, extra=Extra.allow):
+class CellMetadata(BaseModelWithExtras, extra=Extra.allow):
     ...
 
 
-class Cell(BaseModel):
+class CellOutputs(BaseModelWithExtras, extra=Extra.allow):
+    ...
+
+
+class Cell(BaseModelWithExtras):
     cell_type: str
     metadata: CellMetadata
     source: Union[List[str], str]
@@ -39,15 +41,31 @@ class Cell(BaseModel):
     execution_count: Optional[PositiveInt]
 
     def clear_metadata(
-        self, metadata: bool = True, execution_count: bool = True, outputs: bool = False
+        self,
+        cell_metadata: Union[Sequence[str], bool] = True,
+        cell_execution_count: bool = True,
+        cell_outputs: bool = False,
     ):
-        """Clear cell metadata, execution count and outputs"""
-        if metadata:
-            self.metadata = CellMetadata()
-        if outputs and self.cell_type == "code":
-            self.outputs = []
-        if execution_count and self.cell_type == "code":
-            self.execution_count = None
+        """
+        Clear cell metadata, execution count and outputs
+        :param cell_metadata: Either a sequence of metadata fields to remove or `True`
+         to remove all fields
+        :param cell_execution_count: Whether or not to keep the execution count
+        :param cell_outputs: whether or not to keep the cell outputs
+        :return:
+        """
+        if isinstance(cell_metadata, abc.Sequence):
+            self.metadata.remove_fields(cell_metadata)
+        elif cell_metadata:
+            self.metadata.remove_extra_fields()
+
+        if self.cell_type == "code":
+            if cell_outputs:
+                self.outputs = []
+            if cell_execution_count:
+                self.execution_count = None
+        else:
+            self.remove_fields(("outputs", "execution_count"))
 
     @validator("cell_type")
     def cell_has_valid_type(cls, v):
@@ -73,12 +91,11 @@ class JupyterNotebook(BaseModel):
     nbformat_minor: int
     cells: List[Cell]
 
-    def clear_metadata(self, notebook=True, cells=True, **kwargs):
+    def clear_metadata(self, notebook_metadata=True, **kwargs):
         """Clear notebook and cell metadata"""
-        if notebook:
+        if notebook_metadata:
             self.metadata.remove_extra_fields()
-            self.metadata.kernelspec = None
-        if cells:
+        if len(kwargs) > 0:
             _nb_cells = []
             for cell in self.cells:
                 cell.clear_metadata(**kwargs)
