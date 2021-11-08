49963c16120269a4168dcee9824eae9b1d873170
Murilo Cunha <murilo@dataroots.io>
2021-11-08 18:38:57 +0100

    remove kernel and rename arguments

--- databooks/metadata.py
+++ databooks/metadata.py
@@ -5,16 +5,11 @@ from databooks.data_models import JupyterNotebook
 
 
 def clear(
-    read_path: FilePath,
-    write_path: FilePath = None,
-    notebook=True,
-    cells=True,
-    **cell_kwargs
+    read_path: FilePath, write_path: FilePath = None, notebook=True, **cell_kwargs
 ) -> None:
     if write_path is None:
         write_path = read_path
     nb = JupyterNotebook.parse_file(read_path, content_type="json")
-    nb.clear_metadata(notebook=notebook, cells=cells, **cell_kwargs)
-    clean_nb = nb.dict(exclude_none=True)
+    nb.clear_metadata(notebook_metadata=notebook, **cell_kwargs)
     with open(write_path, "w") as out_file:
-        json.dump(clean_nb, fp=out_file, indent=2)
+        json.dump(nb.dict(), fp=out_file, indent=2)
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -9,14 +9,22 @@ def test_metadata_clear():
     read_path = Path("notebooks/small.ipynb")
     write_path = Path("notebooks/clean_small.ipynb")
 
-    clear(read_path=read_path, write_path=write_path, outputs=True)
+    clear(read_path=read_path, write_path=write_path, cell_outputs=True)
 
-    nb = JupyterNotebook.parse_file(write_path, content_type="json")
+    nb = JupyterNotebook.parse_file(path=write_path, content_type="json")
 
     assert write_path.exists()
-    assert nb.metadata.kernelspec is None
     assert all(cell.metadata == CellMetadata() for cell in nb.cells)
     assert all(cell.outputs == [] for cell in nb.cells if cell.cell_type == "code")
     assert all(
         cell.execution_count is None for cell in nb.cells if cell.cell_type == "code"
     )
+
+    # TODO: see comment on `Cell`s `cell_type` validator
+    # assert all(
+    #     not hasattr(cell, "outputs") for cell in nb.cells if cell.cell_type != "code"
+    # )
+    # assert all(
+    #     not hasattr(cell, "execution_count") for cell in nb.cells if cell.cell_type
+    #     != "code"
+    # )
