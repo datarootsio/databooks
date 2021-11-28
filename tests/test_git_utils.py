d90aa78cabcbe6095aa788ab827304caf7af5e51
Murilo Cunha <murilo@dataroots.io>
2021-11-28 22:35:49 +0100

    add function to simulate git repo with merge conflicts - can be reused for different tests

--- tests/test_git_utils.py
+++ tests/test_git_utils.py
@@ -1,8 +1,40 @@
-from pathlib import Path
+from pathlib import Path, PosixPath
 
-from git import Repo
+from git import GitCommandError, Repo
+from pytest import raises
+from pytest_git import GitRepo
 
-from databooks.git_utils import get_repo
+from databooks.git_utils import ConflictFile, get_conflict_blobs, get_repo
+
+
+def init_repo_conflicts(
+    git_repo: GitRepo,
+    filename: Path,
+    contents_main: str,
+    contents_other: str,
+    commit_message_main: str,
+    commit_message_other: str,
+) -> GitRepo:
+    """Create git repo and create a conflict"""
+    git_filepath = git_repo.workspace / filename
+
+    git_repo.run("git checkout -b main")
+    git_repo.run("git commit --allow-empty -m 'Initial commit'")
+
+    git_repo.run("git checkout -b other")
+    git_filepath.write_text(contents_other)
+    git_repo.run(f"git add {filename}")
+    git_repo.run(f"git commit -m '{commit_message_other}'")
+
+    git_repo.run("git checkout main")
+    git_filepath.write_text(contents_main)
+    git_repo.run(f"git add {filename}")
+    git_repo.run(f"git commit -m '{commit_message_main}'")
+
+    with raises(GitCommandError):
+        git_repo.api.git.merge("other")  # merge fails and raises error due to conflict
+
+    return git_repo
 
 
 def test_get_repo() -> None:
