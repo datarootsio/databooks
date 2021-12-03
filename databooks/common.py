91d98ca10cba844c2c54e0447ecebec1b126ee67
Murilo Cunha <murilo@dataroots.io>
2021-12-03 23:38:22 +0100

    add common functions to write notebooks (reusability) and to expand paths (especially used in cli)

--- databooks/common.py
+++ databooks/common.py
@@ -1,7 +1,12 @@
 """Common set of miscellaneous functions"""
+import json
 import logging
+from itertools import chain
+from pathlib import Path
 
 from rich.logging import RichHandler
+
+from databooks.data_models.notebook import JupyterNotebook
 
 
 def get_logger(name: str, level: str = "INFO") -> logging.Logger:
@@ -13,3 +18,25 @@ def get_logger(name: str, level: str = "INFO") -> logging.Logger:
         handlers=[RichHandler(rich_tracebacks=True)],
     )
     return logging.getLogger(name)
+
+
+def write_notebook(nb: JupyterNotebook, path: Path) -> None:
+    """Write notebook to a path"""
+    with path.open("w") as f:
+        json.dump(nb.dict(), fp=f, indent=2)
+
+
+def expand_paths(paths: list[Path], ignore: list[str]) -> list[Path]:
+    """
+    Get paths of existing file from list of directory or file paths
+    :param paths: Paths to consider (can be directories or files)
+    :param ignore: Glob expressions of files to ignore
+    :return: List of existing paths
+    """
+    paths = list(
+        chain.from_iterable(
+            list(path.rglob("*.ipynb")) if path.is_dir() else [path] for path in paths
+        )
+    )
+
+    return [p for p in paths if not any(p.match(i) for i in ignore) and p.exists()]
