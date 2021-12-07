affcad4246d37f89933165a76b794979280cbbb7
Murilo Cunha <murilo@dataroots.io>
2021-12-07 21:15:22 +0100

    fix conflict tests using conflict fiels instead of diff files

--- tests/test_conflicts.py
+++ tests/test_conflicts.py
@@ -1,14 +1,15 @@
-from pathlib import Path, PosixPath
+from pathlib import Path
+from typing import cast
 
-from pytest_git import GitRepo
+from py._path.local import LocalPath
 
-from databooks.conflicts import DiffFile, path2diffs
+from databooks.conflicts import path2conflicts
 from databooks.data_models.notebook import Cell, CellMetadata, NotebookMetadata
-from tests.test_data_models.test_notebook import TestJupyterNotebook
-from tests.test_git_utils import init_repo_conflicts
+from tests.test_data_models.test_notebook import TestJupyterNotebook  # type: ignore
+from tests.test_git_utils import ConflictFile, init_repo_conflicts
 
 
-def test_path2diff(git_repo: GitRepo) -> None:
+def test_path2diff(tmpdir: LocalPath) -> None:
     """Return a DiffFile based on a path and git conflicts"""
     notebook_main = TestJupyterNotebook().jupyter_notebook
     notebook_other = TestJupyterNotebook().jupyter_notebook
@@ -30,23 +31,25 @@ def test_path2diff(git_repo: GitRepo) -> None:
     nb_filepath = Path("test_notebook.ipynb")
 
     git_repo = init_repo_conflicts(
-        git_repo=git_repo,
+        tmpdir=tmpdir,
         filename=nb_filepath,
         contents_main=notebook_main.json(),
         contents_other=notebook_other.json(),
         commit_message_main="Commit message from main",
         commit_message_other="Commit message from other",
     )
+    git_repo.working_dir = cast(Path, git_repo.working_dir)
 
-    diff_files = list(path2diffs(nb_paths=[nb_filepath], repo=git_repo.api))
+    conflict_files = path2conflicts(nb_paths=[nb_filepath], repo=git_repo)
 
-    assert len(diff_files) == 1
+    assert len(conflict_files) == 1
 
-    diff_file = diff_files[0]
-    assert isinstance(diff_file, DiffFile)
-    assert diff_file.filename == PosixPath(nb_filepath)
-    assert diff_file.diff_notebook == notebook_main - notebook_other
+    conflict_file = conflict_files[0]
+    assert isinstance(conflict_file, ConflictFile)
+    assert conflict_file.filename == (git_repo.working_dir / nb_filepath)
+    assert conflict_file.first_contents == notebook_main.json()
+    assert conflict_file.last_contents == notebook_other.json()
 
     # We use git logs for ids, which start with a hash that won't match
-    assert diff_file.first_id.endswith("Commit message from main")
-    assert diff_file.last_id.endswith("Commit message from other")
+    assert conflict_file.first_log.endswith("Commit message from main")
+    assert conflict_file.last_log.endswith("Commit message from other")
