2720c4ec686cc5c8bf059e02cfddd696ce3f316d
Murilo Cunha <murilo@dataroots.io>
2022-10-05 22:39:48 +0200

    move failing test to examples of "bad notebooks" - when the recipe should raise an error

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -49,12 +49,6 @@ class TestCookBookGood:
         recipe = CookBook.no_empty_code.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is True
 
-    def test_error(self) -> None:
-        """Ensure that the notebook's first cell is a markdown cell."""
-        recipe = CookBook.seq_exec.src
-        with resources.path("tests.files", "doesnt_assert.ipynb") as nb:
-            assert affirm(nb_path=nb, exprs=[recipe]) is True
-
 
 class TestCookBookBad:
     """Ensure desired effect for recipes."""
@@ -66,36 +60,42 @@ class TestCookBookBad:
             return nb
 
     def test_has_tags(self) -> None:
-        """Check fail when notebook cells have no flags."""
+        """Check failure when notebook cells have no flags."""
         recipe = CookBook.has_tags.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
 
     def test_has_tags_code(self) -> None:
-        """Check fail when code cells have no flags."""
+        """Check failure when code cells have no flags."""
         recipe = CookBook.has_tags_code.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
 
     def test_max_cells(self) -> None:
-        """Check fail when notebook has more than 128 cells."""
+        """Check failure when notebook has more than 128 cells."""
         recipe = CookBook.max_cells.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
 
     def test_seq_exec(self) -> None:
-        """Check fail when notebook code cells are executed out of order."""
+        """Check failure when notebook code cells are executed out of order."""
         recipe = CookBook.seq_exec.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
 
     def test_seq_increase(self) -> None:
-        """Check fail when notebook code cells are not executed monotonically."""
+        """Check failure when notebook code cells are not executed monotonically."""
         recipe = CookBook.seq_increase.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
 
     def test_startswith_md(self) -> None:
-        """Check fail when notebook's first cell is not a markdown cell."""
+        """Check failure when notebook's first cell is not a markdown cell."""
         recipe = CookBook.startswith_md.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
 
     def test_no_empty_code(self) -> None:
-        """Check fail when notebook contains empty code cells."""
+        """Check failure when notebook contains empty code cells."""
         recipe = CookBook.no_empty_code.src
         assert affirm(nb_path=self.nb, exprs=[recipe]) is False
+
+    def test_seq_exec__clean(self) -> None:
+        """Check failure when notebook cells are not executed."""
+        recipe = CookBook.seq_exec.src
+        with resources.path("tests.files", "clean.ipynb") as nb:
+            assert affirm(nb_path=nb, exprs=[recipe]) is False
