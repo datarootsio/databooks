11d8dc4fb630c55e183446faca16839f0fc57ded
Murilo Cunha <murilo@dataroots.io>
2022-10-05 21:45:38 +0200

    only change logging to DEBUG if verbosity is currently lower (that is, INFO, ...)

--- databooks/logging.py
+++ databooks/logging.py
@@ -21,7 +21,8 @@ def get_logger(name: str) -> logging.Logger:
 def set_verbose(logger: logging.Logger) -> None:
     """Set logger to DEBUG level when user requests verbosity."""
     verbose_level = logging.DEBUG
-    logger.setLevel(verbose_level)
-    logger.debug(
-        f"Verbose mode: setting log level to {logging.getLevelName(verbose_level)}"
-    )
+    if logger.level < verbose_level:
+        logger.setLevel(logging.DEBUG)
+        logger.debug(
+            f"Verbose mode: setting log level to {logging.getLevelName(verbose_level)}"
+        )
