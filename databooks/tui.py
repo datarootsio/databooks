bc55bcdf56f6bd6e58a7b61e9df77d41c10e4f28
Murilo Cunha <murilo@dataroots.io>
2022-10-10 21:41:05 +0200

    add tui basic config and initial tests

---
+++ databooks/tui.py
@@ -0,0 +1,7 @@
+"""Terminal user interface (TUI) helper functions and components."""
+from rich.console import Console
+from rich.theme import Theme
+
+DATABOOKS_TUI = Theme({"in_count": "blue", "out_count": "orange3", "error": "on red"})
+
+default_console = Console(theme=DATABOOKS_TUI)
---
+++ tests/test_tui.py
@@ -0,0 +1,39 @@
+import io
+from importlib import resources
+from textwrap import dedent
+from typing import Iterator
+
+from rich.console import Console
+from rich.segment import Segment
+from typing_extensions import Protocol
+
+from databooks import JupyterNotebook
+
+
+class RichObject(Protocol):
+    """Protocol for mypy static type checking."""
+
+    def __rich_console__(self, *args, **kwargs) -> Iterator[Segment]:
+        """Protocol method that rich-prints object."""
+
+
+with resources.path("tests.files", "demo.ipynb") as nb_path:
+    nb = JupyterNotebook.parse_file(nb_path)
+
+
+def render(obj: RichObject, width: int = 50) -> str:
+    """Render object to string (instead of terminal)."""
+    console = Console(file=io.StringIO(), width=width, legacy_windows=False)
+    console.print(obj)
+    return console.file.getvalue()
+
+
+def test_code_cell():
+    """Prints code cell with no outputs."""
+    assert render(nb.cells[1]) == dedent(
+        """In [1]:
+╭────────────────────────────────────────────────╮
+│ from random import random  # cell with tags    │
+╰────────────────────────────────────────────────╯
+"""
+    )
