ee97b9f7d4cced207a4e2e002c97403fcd81f613
Murilo Cunha <murilo@dataroots.io>
2022-10-11 12:14:19 +0200

    replace custom protocol by the one from `rich` package

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -1,27 +1,16 @@
 import io
 from importlib import resources
 from textwrap import dedent
-from typing import Iterator
 
-from rich.console import Console
-from rich.segment import Segment
-from typing_extensions import Protocol
+from rich.console import Console, ConsoleRenderable
 
 from databooks import JupyterNotebook
-
-
-class RichObject(Protocol):
-    """Protocol for mypy static type checking."""
-
-    def __rich_console__(self, *args, **kwargs) -> Iterator[Segment]:
-        """Protocol method that rich-prints object."""
-
 
 with resources.path("tests.files", "demo.ipynb") as nb_path:
     nb = JupyterNotebook.parse_file(nb_path)
 
 
-def render(obj: RichObject, width: int = 50) -> str:
+def render(obj: ConsoleRenderable, width: int = 50) -> str:
     """Render object to string (instead of terminal)."""
     console = Console(file=io.StringIO(), width=width, legacy_windows=False)
     console.print(obj)
