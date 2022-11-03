55fa5572fdb733f6019673e521c1f18c9f7ff273
Murilo Cunha <murilo@dataroots.io>
2022-11-03 08:58:27 +0100

    only look at paths with notebooks at config and change logic for ignoring paths (Path.match needs to pass each directory explicitly with `**`)

--- databooks/common.py
+++ databooks/common.py
@@ -1,7 +1,7 @@
 """Common set of miscellaneous functions."""
 from itertools import chain
 from pathlib import Path
-from typing import Iterable, List, Optional
+from typing import Iterable, List, Optional, Sequence
 
 from databooks.logging import get_logger
 
@@ -9,7 +9,7 @@ logger = get_logger(__file__)
 
 
 def expand_paths(
-    paths: List[Path], *, ignore: List[str] = ["!*"], rglob: str = "*.ipynb"
+    paths: List[Path], *, ignore: Sequence[str] = ("!*",), rglob: str = "*.ipynb"
 ) -> List[Path]:
     """
     Get paths of existing file from list of directory or file paths.
@@ -20,16 +20,18 @@ def expand_paths(
      existing file paths (i.e.: to retrieve only notebooks)
     :return: List of existing file paths
     """
-    filepaths = list(
+    filepaths = set(
         chain.from_iterable(
-            list(path.rglob(rglob)) if path.is_dir() else [path] for path in paths
+            list(path.resolve().rglob(rglob)) if path.is_dir() else [path]
+            for path in paths
         )
     )
-    valid_filepaths = [
-        p
-        for p in filepaths
-        if not any(p.match(i) for i in ignore) and p.is_file() and p.match(rglob)
-    ]
+    ignored = set(chain.from_iterable(Path.cwd().rglob(i) for i in ignore))
+    ignored = {p.resolve() for p in ignored}
+    logger.debug(
+        f"{len(ignored)} files will be ignored from {len(filepaths)} file paths."
+    )
+    valid_filepaths = [p for p in filepaths - ignored if p.is_file()]
 
     if not valid_filepaths:
         logger.debug(
@@ -58,6 +60,7 @@ def find_obj(
     :return: File path
     """
     finish = finish if finish.is_dir() else finish.parent
+    logger.debug(f"Searching for {obj_name} between {start} and {finish}.")
     if not start.is_dir():
         raise ValueError("Parameter `start` must be a directory.")
 
